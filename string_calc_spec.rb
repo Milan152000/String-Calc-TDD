a39a7eb6697c8483be5df04d49c1a9c0aa4a4e73
Milan Patel <milan1542000@gmail.com>
2025-03-15 20:56:24 +0530

    Sum of both the numbers when 2 numbers are passed comma separated

--- string_calc_spec.rb
+++ string_calc_spec.rb
@@ -15,5 +15,12 @@ describe StringCalc do
       expect(StringCalc.add("395")).to eq(395)
       expect(StringCalc.add("395234")).to eq(395234)  
     end
+
+    # Retruns the sum of both numbers if 2 numbers are passed using a comma as a delimiter
+    it "returns the sum of two numbers" do
+      expect(StringCalc.add("1,2")).to eq(3)
+      expect(StringCalc.add("15,25")).to eq(40)
+      expect(StringCalc.add("1123,562")).to eq(1685)
+    end
   end
 end
